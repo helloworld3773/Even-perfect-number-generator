62318c468386ca4688690a6978b7919478cf0b31
helloworld3773 <zahraa.irfan.sorathia@gmail.com>
2026-04-28 12:03:53 +0400

    Improve prime number detection algorithm
    Refactor prime number checking logic and fix conditions.

--- Primenumbers.cpp
+++ Primenumbers.cpp
@@ -5,10 +5,26 @@ using namespace std;
 int main () {
   double x = 1.000;
 double y = 1.000;
-if (fmod(x,y) != 0) {
+  int z = 0;
+while (y > x) {
+  if (fmod(x,y) != 0 || x = 1) {
 y = y + 1;
+  z = z + 1;
   }
 else {
 cout << "the number " << x << " is not a prime number";
+  }
+}
+  if (z = x - 1 && y = x - 1) {
+    y = 1;
+      z = 0;
+    while (y < (exp2(x))-1) {
+    if (fmod((exp2(x))-1, y != 0 || y = 1) {
+      y = y + 1;
+      z = z + 1;
+    }
+      else {
+      x = x + 1;
+    }
   }
 }
