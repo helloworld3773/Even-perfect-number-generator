ade338ceb4306004706f4fbb4b8aeb413dcc5e2a
helloworld3773 <zahraa.irfan.sorathia@gmail.com>
2026-04-29 11:19:07 +0400

    Finish mersenne prime part and print to terminal

--- DAGENERATORFILE.cpp
+++ DAGENERATORFILE.cpp
@@ -2,10 +2,12 @@
 #include <cmath>
 using namespace std;
 
-int main () {
-  double x = 1.000;
+int main () { 
+double x = 1.000;
 double y = 1.000;
   int z = 0;
+int a = 0;
+double b = 1.000;
 while (y > x) {
   if (fmod(x,y) != 0 || x = 1) {
 y = y + 1;
@@ -16,15 +18,16 @@ x = x + 1;
   }
 }
   if (z = x - 1 && y = x - 1) {
-    y = 1;
-      z = 0;
     while (y < (exp2(x))-1) {
     if (fmod((exp2(x))-1, y != 0 || y = 1) {
-      y = y + 1;
-      z = z + 1;
+      b = b + 1;
+      a = a + 1;
     }
       else {
       x = x + 1;
     }
+      if (a = b = (exp2(x)) - 1 && z = y = x - 1) {
+        cout << exp2(x-1)*(exp2(x)) - 1
+      }
   }
 }
